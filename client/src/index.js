d18e6d64e07ba5601ef4e6b905092f593cfb1345
Aruna Pandian <arunacjeyakumar@gmail.com>
2023-03-13 18:53:21 -0700

    fixed typo

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import store from './store.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
